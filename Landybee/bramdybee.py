5bbfbea387254a20381a8b398d941250ba3421c0
Bram Leo Miles <bram.leo.miles@cern.ch>
2024-07-23 10:54:02 +0200

    Update bramdybee.py, problems calling lanDB twice fixed to get working authorisation. Calling bramDB function automatically authorises user, bramDB.lanDB.<function> calls functions from landybee asking for username and password if authorisation is not valid.

--- Landybee/bramdybee.py
+++ Landybee/bramdybee.py
@@ -7,46 +7,43 @@ import getpass
 import os
 import time
 import os.path as path
-import suds.client
-import suds.xsd.doctor
-import client_internalmodsv4 as landybee
-
-
-client = suds.client.Client(
-            url="https://network.cern.ch/sc/soap/soap.fcgi?v=6&WSDL",
-            doctor=suds.xsd.doctor.ImportDoctor(
-                suds.xsd.doctor.Import("http://schemas.xmlsoap.org/soap/encoding/"),
-            ),
-            cache=None,
-        )
+import client_bramdybee as landybee
 
 class bramDB():
-    
-    def write_env(token) -> str:
+
+    def __init__(self, username: str | None = None, password: str | None = None) -> None:
+        self.landb = landybee.LanDB(username, password)
+        self.authenticate()
+
+    def write_env(self, token) -> str:
         with open('.env', 'w') as file:
             file.write('AUTH_TOKEN=' + token)
         return 'file .env created and token inputted'
 
-    def file_older_than_9half(file): 
+    def file_older_than_9half(self, file): 
+        '''
+        Check whether .env file generated to store the authentication token is older than 9.5 hours.
+        '''
         file_time = path.getmtime(file) 
         if ((time.time() - file_time) / 3600 > 9.5):
             return PermissionError('Authentication key is invalid, enter password and username')
         return ((time.time() - file_time) / 3600 < 9.5)
     
-    def check_authentication(file = '.env', username: str | None = None, password: str | None = None):
-        landb = landybee.LanDB()
-        if os.path.exists(file) and bramDB.file_older_than_9half(file):
+    def authenticate(self, file = '.env', username: str | None = None, password: str | None = None):
+        '''
+        checking authentication token
+        '''
+        if os.path.exists(file) and self.file_older_than_9half(file):
             load_dotenv(find_dotenv(filename= file))
             token = os.getenv('AUTH_TOKEN')
-            # print(token)
-            landb.filltoken(token)
+            self.landb.filltoken(token)
         else:
             print('no valid token present')
             username = input("Enter your username: ")
-            password = getpass.getpass("Enter your password: ")    
-            token = landb.getAuthToken(username, password, 'CERN')
-            bramDB.write_env(token)
-            landb.filltoken(token)
+            password = getpass.getpass("Enter your password: ")
+            token = self.landb.getAuthToken(username, password, 'CERN')
+            self.write_env(token)
+            self.landb.filltoken(token)
             
 
 
