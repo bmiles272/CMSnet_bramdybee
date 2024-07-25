e539f70322595af0c6d8e7a83567e51a57f95aa9
Bram Miles <brmiles@cmsdaq-carbonx1.dyndns.cern.ch>
2024-07-25 17:18:18 +0200

    chnage of CVS file to make life easier, and DictCreate creates dict lists in the format we need them to use with landybee functions

--- Landybee/CSVExtract.py
+++ Landybee/CSVExtract.py
@@ -1,29 +1,39 @@
 import pandas as pd
 
-class CMSNetCSVManager:
+class CMSNetCSVExtract:
 
     def __init__(self) -> None:
-        self.devices_columns = ['Device', 'Serial', 'Zone', 'Manufacturer', 'model', 'Description', 'Tag', 'OS', 'OSversion', 'building', 'floor', 'room', 'Responsible', 'MainUser']
+        self.devices_columns = ['Device', 'Serial', 'Zone', 'Manufacturer', 'model', 'Description', 'Tag', 'OS', 'OSversion', 'building', 'floor', 'room', 'Responsible', 'MainUser', 'ITDHCP']
         self.aliases_columns = ['Device','IFName','Alias']
         self.interfaces_columns = ['Device', 'IFName', 'LinuxIF', 'ServiceName', 'Switch', 'Port', 'Cable', 'Speed']
         self.networks_routes_columns =  ['SourceNetName', 'DestNetName', 'GatewayIP']
         self.networks_columns = ['NetName', 'ServiceName', 'MTU']
         self.switches_special_ports_columns = ['Switch', 'Port', 'Speed', 'PortType']
 
-
     def importfile(self, csvname):
         filepath = f'data/cms/{csvname}.csv'
-        csvfile = pd.read_csv(filepath, comment='#', header=None)
+        csvfile = pd.read_csv(filepath, comment='#')
         return csvfile
 
-    def extractcolumns(self, csvfile, columnnames):
-
-        if len(csvfile.columns) != len(columnnames):
-            raise ValueError("The number of column names does not match the number of columns in the csv file.")
+    def extractcolumns(self, csv: str, columnnames):
+        csvfile = self.importfile(csv)
+        self.checkcsv(csvfile, columnnames)
 
         for index in range(len(csvfile.columns)):
             variable_name = columnnames[index]
             column_data = csvfile.iloc[:, index].tolist()
             globals()[variable_name] = column_data
-
         return print(f'You can now call the data by variable names: {columnnames}')
+    
+    def extractelement(self, csv: str, indx_row, indx_col):
+        csvfile = self.importfile(csv)
+        extracted_elements = csvfile.iloc[indx_row, indx_col]
+        return extracted_elements
+
+    def extractrow(self, csv: str, rowindex):
+        csvfile = self.importfile(csv)
+        extractedrow = csvfile.iloc[rowindex, :]
+        return extractedrow
+    
+hey = CMSNetCSVExtract()
+d = hey.importfile('devices')
