88b80815f045bd9d07b20ea76da703ce9ad50ee5
Bram Miles <brmiles@cmsdaq-carbonx1.dyndns.cern.ch>
2024-08-23 09:55:28 +0200

    extract function to populate dhcp and dns files

--- Landybee/CMSNet_ng_extract.py
+++ Landybee/CMSNet_ng_extract.py
@@ -9,24 +9,50 @@ for dchp:
 import os
 import shutil
 import re
-
+import json
+import argparse
+from bramdybee import bramDB
+from ConvSUDStoDict import SUDS2Dict
+bramdb = bramDB()
+conv = SUDS2Dict()
 
 
 class cmsnet_extract:
 
-    def __init__(self, domain) -> None:
+    def __init__(self, domain: str) -> None:
         
         ip_ranges_cms = ("10.176.", "10.184.32.")
         ip_ranges_904 = ("10.192.")
-        self.domain = domain
+        self.domain = domain.upper()
+        self.cmsIF_domain = ".CERN.CH"
 
         domains = ("cms", "cms904")
+
+        self.dhcp = []
 
         #check whether domain given is valid, if not give valid one.
         if domain not in domains:
             print(f"The domain specified {domain} is not a valid domain. Valid domains are: {domains}")
             exit()
 
+    def domain_devices(self, domain):
+        dmn_dict = {"Domain": domain}
+        try:
+            domain_list = bramdb.landb.searchDevice(dmn_dict)
+        except Exception as e:
+            print(f"ERROR obtaining list of device names from domain {domain}")
+        return domain_list
+    
+    def find_dict_by_entry(self, dict_list, key, value):
+        for d in dict_list:
+        # Ensure that d is not None and the key exists in the dictionary
+            if d is not None:
+                dict_value = d.get(key)
+                if value is None and dict_value is None:
+                    return None
+                elif isinstance(dict_value, str) and isinstance(value, str) and dict_value.upper() == value.upper():
+                    return d
+        return None
 
     def create_files(self, dhcp):  #does not necesarily need to be a function just needs to be created
         #generate the files
@@ -39,7 +65,89 @@ class cmsnet_extract:
         for i in sorted(dhcp.keys()):
             with open(f"build.var/dhcp/{self.domain}.{i}.dhcp", "w") as dhcp_fh:
                 dhcp_fh.write(dhcp[i])
+    
+    def split_list(self, lst, section_length):
+        return [lst[i:i + section_length] for i in range(0, len(lst), section_length)]
 
+    def process_normal_cases(self):
+        self.devices_by_group = {
+        'dcs': [],
+        'cc8': [],
+        'juniper': [],
+        'misc': []
+    }
+
+        #cc8 versions condition list
+        cc8_versions = ['CC8', 'RHEL8', 'ALMA9', 'RHEL9', 'ALMA9']
+
+        #first we find all devices in domain
+        devices = self.domain_devices(self.domain)
+
+        #next we get info arrays for all devices in steps of 50
+        split_devices = self.split_list(devices, 200)
+
+        for sub_devices in split_devices:
+            try:
+                device_info = bramdb.landb.getDeviceInfoArray(sub_devices)
+                for single_devinfo in device_info:
+                    #turn each suds object into usable dictionary
+                    deviceinfo_str = conv.sobject_to_json(single_devinfo)
+                    devinfo_dict = json.loads(deviceinfo_str)
+
+                    #start defining all things needed to populate DHCP files
+                    dev_name = devinfo_dict.get("DeviceName")
+                    IF_name = dev_name + self.cmsIF_domain
+                    IFs = devinfo_dict.get("Interfaces")
+                    correct_IF = self.find_dict_by_entry(IFs, 'Name', IF_name)
+                    os = devinfo_dict.get("OperatingSystem")
+
+                    if correct_IF:
+                        ip = correct_IF.get("IPAddress")
+                        # print(ip)
+                        boundinterface = correct_IF.get("BoundInterfaceCard")
+                        mac = correct_IF.get("HardwareAddress") if boundinterface else None
+                        # print(mac)
+                        subnetmask = correct_IF.get("SubnetMask")
+                        # print(subnetmask)
+                        defaultgateway = correct_IF.get("DefaultGateway")
+                        # print(defaultgateway)
+
+                        #append the device names to each respective file
+                        if os.get("Name") == "WINDOWS":
+                            self.devices_by_group['dcs'].append(dev_name)
+                        if os.get("Version") in cc8_versions:
+                            self.devices_by_group['cc8'].append(dev_name)
+                        if devinfo_dict.get("Manufacturer") == "JUNIPER":
+                            self.devices_by_group['juniper'].append(dev_name)
+                        else:
+                            self.devices_by_group['misc'].append(dev_name)
+                    else:
+                        print(dev_name)
+                        ip = mac = subnetmask = defaultgateway = None
+
+                    
+                    
+                    
+            except Exception as e:
+                print(f"ERROR getting device info: {e}")
+
+        print("self.devices_by_group created")
+
+        for group, device_list in self.devices_by_group.items():
+            for device in device_list:
+                try:
+                    DHCP = self.add_dhcp(device_name= device,
+                                  IP= ip,
+                                  MAC= mac,
+                                  group= group,
+                                  netmask= subnetmask,
+                                  def_gateway= defaultgateway,
+                                  dhcp= self.devices_by_group)
+                    
+                except Exception as e:
+                    print(f"ERROR retreiving DHCP config: {e}") 
+        
+        print(DHCP["juniper"])
 
 
     def broadcast_address(self, mask, def_gateway, ip):
@@ -65,8 +173,8 @@ class cmsnet_extract:
 
     #might need to initialise dhcp outside the function populated by device names within groups within the domain
 
-    def add_dhcp(self, device_name, IP, MAC, group, netmask, broadcast, def_gateway, dhcp):
-        entries = [device_name, IP, MAC, group, netmask, broadcast, def_gateway]
+    def add_dhcp(self, device_name, IP, MAC, group, netmask, def_gateway, dhcp):
+        entries = [device_name, IP, MAC, group, netmask, def_gateway, dhcp]
         if any(arg is None for arg in entries):
             print("Missing arguments in: 'add_dhcp'")
             return
